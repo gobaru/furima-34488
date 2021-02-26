9d0a9e4de1f6809db6ffa070aeb5012ea6238456
gobaru <gobaru.1993@gmail.com>
2021-02-26 15:58:28 +0900

    レビュー修正①

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :redirect_item, only: [:edit, :update]
+  before_action :redirect_item, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
