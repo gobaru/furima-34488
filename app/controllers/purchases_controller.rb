966e57159fcde56b3a0ff6e90aa4e9b266e1e78c
gobaru <gobaru.1993@gmail.com>
2021-03-02 11:58:16 +0900

    モデルの単体テストコード作成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,36 +1,31 @@
-  class PurchasesController < ApplicationController
-    before_action :authenticate_user!, only: :index
-    before_action :set_item, only: [:index, :create]
-  
-    def index
-      @purchase_shipping_address = PurchaseShippingAddress.new
-      if current_user.id == @item.user_id || @item.purchase.present?
-        redirect_to root_path
-      end
-    end
-  
-    def create
-      # binding.pry
-      @purchase_shipping_address = PurchaseShippingAddress.new(purchase_params)
-      if @purchase_shipping_address.valid?
-        @purchase_shipping_address.save
-        redirect_to root_path
-      else
-        render :index
-      end
-    end
+class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: :index
+  before_action :set_item, only: [:index, :create]
 
-  
-  
-    private
-  
-    def purchase_params
-      params.require(:purchase_shipping_address).permit(:postal_code, :prefecture_id, :municipalities, :address, :building_name, :phone_num).merge(user_id: current_user.id, item_id: @item.id)
-    end
-  
-    def set_item
-      @item = Item.find(params[:item_id])
-    end
-    
-  
+  def index
+    @purchase_shipping_address = PurchaseShippingAddress.new
+    redirect_to root_path if current_user.id == @item.user_id || @item.purchase.present?
   end
+
+  def create
+    @purchase_shipping_address = PurchaseShippingAddress.new(purchase_params)
+    if @purchase_shipping_address.valid?
+      @purchase_shipping_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_shipping_address).permit(:postal_code, :prefecture_id, :municipalities, :address, :building_name, :phone_num).merge(
+      user_id: current_user.id, item_id: @item.id
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+end
---
+++ spec/models/purchase_shipping_address_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+
+RSpec.describe PurchaseShippingAddress, type: :model do
+  before do
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item, user_id: user.id)
+    @purchase_shipping_address = FactoryBot.build(:purchase_shipping_address, user_id: user.id, item_id: item.id)
+    sleep 0.1
+  end
+
+  describe '商品の購入' do
+    context '商品購入ができるとき' do
+      it '@purchase_shipping_addressに問題がないとき' do
+        expect(@purchase_shipping_address).to be_valid
+      end
+
+      it 'building_nameが空でも登録できる' do
+        @purchase_shipping_address.building_name = ''
+        expect(@purchase_shipping_address).to be_valid
+      end
+    end
+
+    context '商品購入ができないとき' do
+      it 'postal_codeが空では登録できない' do
+        @purchase_shipping_address.postal_code = ''
+        @purchase_shipping_address.valid?
+        expect(@purchase_shipping_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+
+      it 'postal_codeにハイフンがないと登録できない' do
+        @purchase_shipping_address.postal_code = 1_234_567
+        @purchase_shipping_address.valid?
+        expect(@purchase_shipping_address.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
+      end
+
+      it 'prefecture_idが空では登録できない' do
+        @purchase_shipping_address.prefecture_id = ''
+        @purchase_shipping_address.valid?
+        expect(@purchase_shipping_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it 'municipalitiesが空では登録できない' do
+        @purchase_shipping_address.municipalities = ''
+        @purchase_shipping_address.valid?
+        expect(@purchase_shipping_address.errors.full_messages).to include("Municipalities can't be blank")
+      end
+
+      it 'addressが空では登録できない' do
+        @purchase_shipping_address.address = ''
+        @purchase_shipping_address.valid?
+        expect(@purchase_shipping_address.errors.full_messages).to include("Address can't be blank")
+      end
+
+      it 'phone_numが空では登録できない' do
+        @purchase_shipping_address.phone_num = ''
+        @purchase_shipping_address.valid?
+        expect(@purchase_shipping_address.errors.full_messages).to include("Phone num can't be blank")
+      end
+
+      it 'phone_numが数字でないと登録できない' do
+        @purchase_shipping_address.phone_num = 'アイウエオ'
+        @purchase_shipping_address.valid?
+        expect(@purchase_shipping_address.errors.full_messages).to include('Phone num Half-width number')
+      end
+
+      it 'phone_numが11桁以内でないと登録できない' do
+        @purchase_shipping_address.phone_num = '090000000000'
+        @purchase_shipping_address.valid?
+        expect(@purchase_shipping_address.errors.full_messages).to include('Phone num maximum 11 characters')
+      end
+    end
+  end
+end
